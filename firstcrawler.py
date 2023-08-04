b9bac33c6a128e3495b69ba43f64ac54d13580c3
Joshua Chen <thaiguofeilan@gmail.com>
2023-08-04 23:58:47 +0800

    增加了使用者輸入條件以及一些小功能

--- firstcrawler.py
+++ firstcrawler.py
@@ -1,7 +1,7 @@
 import urllib.request as req #用於處理URL請求
 import bs4 #用於解析HTML
 
-def getData(url, count, seen):
+def getData(url, count, keyword, seen):
     #這裡是要假裝程式是真人對瀏覽器發送請求 
     request = req.Request(url, headers={
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
@@ -20,23 +20,28 @@ def getData(url, count, seen):
             title_text = title.a.string
             #這裡是為了讓它不要輸出同樣的標題用的
             if title_text not in seen:
-                with open("chain.txt", mode="a", encoding="utf-8") as file:
-                    file.write(title_text + "\n")
-                seen.add(title_text) #把已經出現過的標題放進seen的集合裡
+                if keyword in title_text:
+                    with open("chain.txt", mode="a", encoding="utf-8") as file:
+                        file.write(title_text + "\n")
+                    seen.add(title_text) #把已經出現過的標題放進seen的集合裡
     
     #一頁結束 準備下一頁的連結            
     count += 1 
     nextpage = root.find("a", string=f"{count}")
-    return nextpage["href"]
+    if nextpage is not None: 
+        return nextpage["href"]
+    else:
+        return None
 
 #我想爬的網站
-pageurl = "https://abmedia.io/blog"    
+pageurl = "https://abmedia.io/blog"   
+ 
+pages = int(input("請輸入想要幾頁 : "))
+keyword = input("請輸入想要的關鍵字 : ")
 
 count = 1
 seen = set() #為了不讓輸出同樣內容而設置的集合
 
-while count <= 3:
-    pageurl = getData(pageurl, count, seen)
+while count <= pages and pageurl is not None:
+    pageurl = getData(pageurl, count, keyword, seen)
     count += 1
-
-#試試看
